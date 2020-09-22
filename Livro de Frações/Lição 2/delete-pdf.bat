343854b1f200f94e5c17810297191fabc56a222b
Tarso Boudet Caldas <tarsobcaldas@gmail.com>
2020-09-22 16:33:16 -0300

    Adição de atividades
    Colocadas as atividades das Lições 3 e 4 do Livro de Frações e dos capítulos de Função Afim e A Natureza da Estatística

--- Livro de Frações/Lição 2/delete-pdf.bat
+++ Livro de Frações/Lição 2/delete-pdf.bat
@@ -1,33 +1,16 @@
 @@ECHO OFF
 CLS
 
-SET found=0
-ECHO Enter the file extension you want to delete...
-SET /p ext="> "
 
-IF EXIST *.%ext% (           rem Check if there are any in the current folder :)
-  DEL *.%ext%
-  SET found=1
+IF EXIST *.pdf (           rem Check if there are any in the current folder :)
+  DEL *.pdf
 )
 FOR /D /R %%G IN ("*") DO (  rem Iterate through all subfolders
   IF EXIST %%G CD %%G
-  IF EXIST *.%ext% (
-    DEL *.%ext%
+  IF EXIST *.pdf (
+    DEL *.pdf
     SET found=1
   )
 )
 
-IF %found%==1 (
-  ECHO.
-  ECHO Deleted all .%ext% files.
-  ECHO.
-) ELSE (
-  ECHO.
-  ECHO There were no .%ext% files.
-  ECHO Nothing has been deleted.
-  ECHO.
-)
-
-PAUSE
-EXIT
 EXIT
